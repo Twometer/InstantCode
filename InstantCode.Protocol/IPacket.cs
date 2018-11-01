788356f3355c2c071726ef858915562f08506daf
Twometer <twometer@outlook.de>
2018-11-01 18:51:59 +0100

    Linked projects together

---
+++ InstantCode.Protocol/IPacket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstantCode.Protocol
+{
+    interface IPacket
+    {
+
+        void Read(PacketBuffer buffer);
+
+        void Write(PacketBuffer buffer);
+
+    }
+}
---
+++ InstantCode.Protocol/PacketBuffer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstantCode.Protocol
+{
+    public class PacketBuffer
+    {
+    }
+}
